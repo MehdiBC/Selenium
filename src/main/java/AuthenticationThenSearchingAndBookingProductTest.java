e0f68e479737be3ad0b1a3c1c3fb4f65afcb0137
Mehdi <mehdi.b.chikha@gmail.com>
2021-12-19 06:39:50 +0100

    initial commit

--- src/main/java/AuthenticationThenSearchingAndBookingProductTest.java
+++ src/main/java/AuthenticationThenSearchingAndBookingProductTest.java
@@ -1,37 +1,67 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class AuthenticationThenSearchingAndBookingProductTest {
-    public static void main(String[] args) throws InterruptedException {
+
+
+    public static WebDriver createDriver() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        return new ChromeDriver();
+    }
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
-        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(2));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    public static void configureDriver(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
 
-        driver.get("https://www.tunisianet.com.tn/");
-        driver.manage().window().setSize(new Dimension(1552, 840));
+    public static void navigateTo(WebDriver driver, String url) {
+        driver.navigate().to(url);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = createDriver();
+
+        configureDriver(driver);
+        // Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(2));
+
+        navigateTo(driver, "https://www.tunisianet.com.tn/");
+
         Thread.sleep(2000);
+
         driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg")).click();
         driver.findElement(By.cssSelector(".user-down span")).click();
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("mehdi.benchikha@insat.u-carthage.tn");
-        driver.findElement(By.name("password")).click();
-        driver.findElement(By.name("password")).sendKeys("123456789");
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.click();
+        email.sendKeys("mehdi.benchikha@insat.u-carthage.tn");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.click();
+        password.sendKeys("123456789");
+
         driver.findElement(By.id("submit-login")).click();
-        driver.findElement(By.id("search_query_top")).click();
-        driver.findElement(By.id("search_query_top")).sendKeys("MacBook M1 13.3");
-        Thread.sleep(20000);
-        driver.findElement(By.linkText("PC PORTABLE APPLE MACBOOK PRO M1 13.3\" / 256 GO SSD / GRIS SIDÉRAL")).click();
+
+
+        WebElement searchInput = driver.findElement(By.id("search_query_top"));
+        searchInput.click();
+        searchInput.sendKeys("MacBook M1 13.3");
+
+        driver.findElement(By.cssSelector(".button-search > .nav-link")).click();
+
+        driver.findElement(By.linkText("PC Portable Apple MacBook Pro M1 13.3\" / 256 Go SSD / Gris Sidéral")).click();
         driver.findElement(By.cssSelector(".add-to-cart")).click();
+        Thread.sleep(2000);
         driver.findElement(By.cssSelector(".btn-block:nth-child(1)")).click();
-        Thread.sleep(10000);
-        driver.close();
+        Thread.sleep(2000);
+
+        driver.quit();
     }
 }
